def5c0074d9ea19eed50af0f7690dc8d5193974c
Sebaztianin <seba_braga01@hotmail.com>
2022-12-26 23:40:04 -0300

    Mejoras al home

--- app.js
+++ app.js
@@ -9,5 +9,25 @@ app.listen(PORT, ()=>{
 });
 
 app.get('/', (req, res)=>{
-    res.sendFile(__dirname + '/views/home.html');
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/login', (req, res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/products', (req, res)=>{
+    res.sendFile(__dirname + '/views/products.html');
+});
+
+app.get('/productDetail', (req, res)=>{
+    res.sendFile(__dirname + '/views/productDetail.html');
+});
+
+app.get('/productCart', (req, res)=>{
+    res.sendFile(__dirname + '/views/productCart.html');
+});
+
+app.get('/login', (req, res)=>{
+    res.sendFile(__dirname + '/views/login.html');
 });
