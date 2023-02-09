c8df8a53d2c8aa4b1e9922bcf248c568cbd5b057
Sebaztianin <seba_braga01@hotmail.com>
2023-02-08 21:42:23 -0300

    Registro de usuarios listo

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,8 +1,70 @@
+/* Importamos los módulos a utilizar */
+const fs = require('fs');
+const path = require('path');
+const bcrypt = require('bcryptjs');
+
+/* Creamos el path de usuarios y recuperamos el JSON parseado en users */
+const usersFilePath = path.join(__dirname, '../data/users.json');
+const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+
+/* Importamos las validaciones */
+const { validationResult } = require('express-validator');
+
 /* Creamos el módulo y exportamos */
-let productsController = {
-    index: function(req, res) {
+let usersController = {
+
+    index: function (req, res) {
         res.render('users/login');
+    },
+
+    register: function (req, res) {
+
+        // Recuperamos resultados de la validación
+        let errors = validationResult(req);
+        let passwordVerification;
+
+        // Consultamos si no existen errores
+        if (errors.isEmpty()) {   // No hay errores, continuamos...
+
+            // Obtener id
+            let id = users[users.length - 1].id + 1;
+            let newUser = {};
+
+            // Agregar datos del usuario a la variable
+            newUser.id = id;
+            newUser.firstName = req.body.firstName;
+            newUser.lastName = req.body.lastName;
+            newUser.email = req.body.email;
+            newUser.category = 'cliente';
+            newUser.password = bcrypt.hashSync(req.body.password, 10);
+            newUser.image = req.file.filename;
+
+            // Agregar usuario a la lista de usuarios
+            let usersNew = users;
+            usersNew.push(newUser);
+
+            // Sobreescribir JSON con producto agregado
+            fs.writeFileSync(usersFilePath, JSON.stringify(usersNew));
+
+            // Redireccionamos al main (deberíamos redirigir al perfil del usuario)
+            res.redirect('/');
+
+        } else {   // Hay errores, volvemos al formulario
+
+            // Eliminamos archivo mal cargado si es que se seleccionó un archivo en el formulario y existe tal archivo
+            if (req.file) {
+                if (fs.existsSync(path.join(__dirname, '../../public/images/users/', req.file.filename))) {
+                    fs.unlinkSync(path.join(__dirname, '../../public/images/users/', req.file.filename));
+                }
+            }
+
+            // Volvemos al formulario con los errores y los datos viejos
+            res.render('users/login', { errors: errors.array(), old: req.body });
+
+        }
+
     }
+
 };
 
-module.exports = productsController;
+module.exports = usersController;
--- src/routers/users.js
+++ src/routers/users.js
@@ -2,13 +2,58 @@
 /* Importamos módulos */
 const express = require('express');
 const app = express();
+const path = require('path');
 
 /* Importamos módulos de ruteo */
 const usersController = require('../controllers/usersController'); 
+
+/* Importamos y configuramos las validaciones */
+const { body } = require('express-validator');
+
+/* Validaciones del formulario de registración */
+let registerForm = [
+	body('firstName').notEmpty().withMessage('El nombre no puede estar vacío.').bail()
+		.isLength({ min: 2 }).withMessage('El nombre no puede tener un largo menor a 2.'),
+	body('lastName').notEmpty().withMessage('El apellido no puede estar vacío.').bail()
+    .isLength({ min: 2 }).withMessage('El apellido no puede tener un largo menor a 2.'),
+	body('email').notEmpty().withMessage('El email no puede estar vacío.').bail()
+		.isEmail().withMessage('Ingrese un correo válido.'),
+    body('password').notEmpty().withMessage('Debe ingresar una contraseña.').bail()
+    .isLength({ min: 8 }).withMessage('La contraseña debe contener por lo menos 8 caracteres.'),
+	body('passwordConfirmation').custom((value, { req }) => {
+		if (value != req.body.password) {
+			throw new Error('Las contraseñas no coinciden.');
+		}
+		return true;
+	}),
+	body('avatar').custom((value, { req }) => {
+		if (!req.file) {
+			throw new Error('Se requiere una imagen.');
+		} else if (path.extname(req.file.filename) != '.jpg') {
+			throw new Error('Se requiere un archivo de extensión .jpg.');
+		}
+		return true;
+	})
+];
+
+/* Importamos y configuramos Multer para las imágenes */
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, path.join(__dirname, '../../public/images/users'));
+	},
+	filename: function (req, file, cb) {
+		cb(null, `img-${path.parse(file.originalname).name}-${Date.now()}${path.extname(file.originalname)}`);
+	}
+});
+
+const uploadFile = multer({ storage: storage });
 
 /* Creamos el módulo y definimos las rutas para main */
 let router = express.Router();
 
 router.get('/login', usersController.index);
+router.post('/register', uploadFile.single('avatar'), registerForm, usersController.register);
 
 module.exports = router;
