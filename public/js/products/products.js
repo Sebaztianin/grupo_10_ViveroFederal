92cd9304162c8d660702844657a9ef18dd22e32e
Sebaztianin <seba_braga01@hotmail.com>
2023-03-31 10:26:54 -0300

    products.js reescrito con URLSearchParams

--- public/js/products/products.js
+++ public/js/products/products.js
@@ -1,16 +1,6 @@
 
-// Convertimos el querystring en un objeto
-let querystring = window.location.search.substring(1);
-let query = querystring ? JSON.parse('{"' + decodeURI(querystring).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"') + '"}') : '';
-
-// Rearmo querystring sin el parámetro de la página
-querystring = '';
-Object.keys(query).forEach(param => {
-    if (param != 'page') {querystring = querystring + param + '=' + query[param] + '&'}
-});
-
-// Saco el último &
-querystring = querystring.substring(0, querystring.length - 1);
+// Trabajamos con la query
+let query = new URLSearchParams(location.search);
 
 // Obtengo los elementos a de los filtros
 let categoriesItems = document.querySelectorAll('div.filters div.categories ul li a');
@@ -19,16 +9,16 @@ let sizesItems = document.querySelectorAll('div.filters div.sizes ul li a');
 let clean = document.querySelector('div.filters a.clean');
 
 // Trabajo con las categorías
-if (!query.category) { // No hay un parámetro seleccionado
+if (!query.has('category')) { // No hay un parámetro seleccionado
 
     categoriesItems.forEach(category => {
 
         // Agrego al href lo que sigue
-        if (querystring) {
-            category.setAttribute('href', '/products?category=' + category.getAttribute('href') + '&' + querystring);
-        } else {
-            category.setAttribute('href', '/products?category=' + category.getAttribute('href'));
-        }
+        query.append('category', category.getAttribute('href'));
+        category.setAttribute('href', '/products?' + query.toString());
+
+        // Saco de query para operar con el que sigue
+        query.delete('category');
 
     });
 
@@ -37,7 +27,7 @@ if (!query.category) { // No hay un parámetro seleccionado
     categoriesItems.forEach(category => {
 
         // Resalto el elemento seleccionado y escondo los demás
-        if (query.category == category.getAttribute('href')) {
+        if (query.get('category') == category.getAttribute('href')) {
             category.style.fontWeight = 'bold';
             category.setAttribute('href', '#');
         } else {
@@ -49,16 +39,16 @@ if (!query.category) { // No hay un parámetro seleccionado
 }
 
 // Trabajo con los colores
-if (!query.color) { // No hay un parámetro seleccionado
+if (!query.has('color')) { // No hay un parámetro seleccionado
 
     colorsItems.forEach(color => {
 
         // Agrego al href lo que sigue
-        if (querystring) {
-            color.setAttribute('href', '/products?color=' + color.getAttribute('href') + '&' + querystring);
-        } else {
-            color.setAttribute('href', '/products?color=' + color.getAttribute('href'));
-        }
+        query.append('color', color.getAttribute('href'));
+        color.setAttribute('href', '/products?' + query.toString());
+
+        // Saco de query para operar con el que sigue
+        query.delete('color');
 
     });
 
@@ -67,7 +57,7 @@ if (!query.color) { // No hay un parámetro seleccionado
     colorsItems.forEach(color => {
 
         // Resalto el elemento seleccionado y escondo los demás
-        if (query.color == color.getAttribute('href')) {
+        if (query.get('color') == color.getAttribute('href')) {
             color.style.fontWeight = 'bold';
             color.setAttribute('href', '#');
         } else {
@@ -79,16 +69,16 @@ if (!query.color) { // No hay un parámetro seleccionado
 }
 
 // Trabajo con los tamaños
-if (!query.size) { // No hay un parámetro seleccionado
+if (!query.has('size')) { // No hay un parámetro seleccionado
 
     sizesItems.forEach(size => {
 
         // Agrego al href lo que sigue
-        if (querystring) {
-            size.setAttribute('href', '/products?size=' + size.getAttribute('href') + '&' + querystring);
-        } else {
-            size.setAttribute('href', '/products?size=' + size.getAttribute('href'));
-        }
+        query.append('size', size.getAttribute('href'));
+        size.setAttribute('href', '/products?' + query.toString());
+
+        // Saco de query para operar con el que sigue
+        query.delete('size');
 
     });
 
@@ -97,7 +87,7 @@ if (!query.size) { // No hay un parámetro seleccionado
     sizesItems.forEach(size => {
 
         // Resalto el elemento seleccionado y escondo los demás
-        if (query.size == size.getAttribute('href')) {
+        if (query.get('size') == size.getAttribute('href')) {
             size.style.fontWeight = 'bold';
             size.setAttribute('href', '#');
         } else {
@@ -109,36 +99,41 @@ if (!query.size) { // No hay un parámetro seleccionado
 }
 
 // Hago que el botón limpiar filtros mantenga lo tipeado por el usuario
-if (query.search) {
-    clean.setAttribute('href', '/products?search=' + query.search)
+if (query.has('search')) {
+    clean.setAttribute('href', '/products?search=' + query.get('search'));
 }
 
 // Obtengo los elementos de la paginación
 let prevPage = document.querySelector('div.products div.pages a.prevPage');
 let nextPage = document.querySelector('div.products div.pages a.nextPage');
 
+// Saco el atributo page del query
+query.delete('page');
+
 // Si es igual a 0, osea, no hay otro elemento, desactivo el botón. Si es distinto de 0, seteo href
 if (prevPage.getAttribute("href") == 0) {
     prevPage.style.display = "none";
 } else {
-    
-    if (querystring) {
-        prevPage.setAttribute('href', '/products?page=' + prevPage.getAttribute("href") + '&' + querystring);
-    } else {
-        prevPage.setAttribute('href', '/products?page=' + prevPage.getAttribute("href"));
-    }
-    
-}
+
+    // Agrego al href lo que sigue
+    query.append('page', prevPage.getAttribute('href'));
+    prevPage.setAttribute('href', '/products?' + query.toString());
+
+    // Saco de query para operar con el que sigue
+    query.delete('page');
+
+} 
 
 // Si es igual a 0, osea, no hay otro elemento, desactivo el botón. Si es distinto de 0, seteo href
 if (nextPage.getAttribute('href') == 0) {
     nextPage.style.display = 'none';
 } else {
-    
-    if (querystring) {
-        nextPage.setAttribute('href', '/products?page=' + nextPage.getAttribute("href") + '&' + querystring);
-    } else {
-        nextPage.setAttribute('href', '/products?page=' + nextPage.getAttribute("href"));
-    }
 
-}
+    // Agrego al href lo que sigue
+    query.append('page', nextPage.getAttribute('href'));
+    nextPage.setAttribute('href', '/products?' + query.toString());
+
+    // Saco de query para operar con el que sigue
+    query.delete('page');
+
+} 
