195d24fe9dcf23cbe0afb3aefa63b8c7b583732b
Sebastián Braga Méndez <seba_braga01@hotmail.com>
2023-06-04 14:06:08 +1000

    Corrigiendo problemas de seguridad
    Las credenciales no pueden setearse en el códgio, debemos usar las variables del hosting

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,37 +2,6 @@ module.exports = {
 
     "development": {
         "username": "root",
-        "password": "RuhaFlZI7GZUfUm4QVyX",
-        "database": "vivero_federal_db",
-        "host": "containers-us-west-159.railway.app",
-        "port": "6166",
-        "dialect": "mysql"
-    },
-
-    "test": {
-        "username": "root",
-        "password": "RuhaFlZI7GZUfUm4QVyX",
-        "database": "vivero_federal_db",
-        "host": "containers-us-west-159.railway.app",
-        "port": "6166",
-        "dialect": "mysql"
-    },
-
-    "production": {
-        "username": "root",
-        "password": "RuhaFlZI7GZUfUm4QVyX",
-        "database": "vivero_federal_db",
-        "host": "containers-us-west-159.railway.app",
-        "port": "6166",
-        "dialect": "mysql"
-    }
-
-}
-
-/* 
-
-    "development": {
-        "username": "root",
         "password": null,
         "database": "vivero_federal_db",
         "host": "127.0.0.1",
@@ -54,5 +23,6 @@ module.exports = {
         "host": "127.0.0.1",
         "dialect": "mysql"
     }
-    
-*/
+
+}
+
