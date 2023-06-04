43ba628f04e1325bd7a6070433d46b56086fdf4d
Sebastián Braga Méndez <seba_braga01@hotmail.com>
2023-06-04 13:10:59 +1000

    Pruebas railway

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,34 +1,40 @@
 module.exports = {
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": "root",
-    "password": "BHVav4kvhEBi7lIhkKBK",
-    "database": "vivero_federal_db",
-    "host": "containers-us-west-50.railway.app",
-    "port": "7757",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
+
+    "development": {
+        "username": "root",
+        "password": "BHVav4kvhEBi7lIhkKBK",
+        "database": "vivero_federal_db",
+        "host": "containers-us-west-50.railway.app",
+        "port": "7757",
+        "dialect": "mysql"
+    },
+    
+    "test": {
+        "username": "root",
+        "password": null,
+        "database": "database_test",
+        "host": "127.0.0.1",
+        "dialect": "mysql"
+    },
+
+    "production": {
+        "username": "root",
+        "password": null,
+        "database": "database_production",
+        "host": "127.0.0.1",
+        "dialect": "mysql"
+    }
+
 }
 
 /* 
-  "test": {
+
+"development": {
     "username": "root",
     "password": null,
-    "database": "database_test",
+    "database": "vivero_federal_db",
     "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
+    "dialect": "mysql" 
+}
+    
 */
