04e5599c2584373736b5996a334405113188e427
Sebastián Braga Méndez <seba_braga01@hotmail.com>
2023-06-04 13:07:13 +1000

    Pruebas railway

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,17 +1,17 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "BHVav4kvhEBi7lIhkKBK",
-    "database": "vivero_federal_db",
-    "host": "containers-us-west-50.railway.app",
-    "port": "7757",
+    "password": null,
+    "database": "database_development",
+    "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
     "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "password": "BHVav4kvhEBi7lIhkKBK",
+    "database": "vivero_federal_db",
+    "host": "containers-us-west-50.railway.app",
+    "port": "7757",
     "dialect": "mysql"
   },
   "production": {
@@ -23,9 +23,12 @@ module.exports = {
   }
 }
 
-/* "development": {
+/* 
+  "test": {
     "username": "root",
     "password": null,
-    "database": "vivero_federal_db",
+    "database": "database_test",
     "host": "127.0.0.1",
-    "dialect": "mysql" */
+    "dialect": "mysql"
+  },
+*/
